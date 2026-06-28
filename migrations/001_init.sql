4579eee0a4ff8fa932d00b41288fc41da9dac038
Priyanshu Dangare <priyanshu@ente.com>
2026-06-28 22:11:12 +0530

    init

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -3,15 +3,15 @@ create table if not exists share (
     expires_at text not null,
     passwd text not null,
     metadata text not null default '{}',
-    anyone_can_write boolean not null default false
+    anyone_can_write integer not null default 0
 
 ) strict, without rowid;
 
 create table if not exists blob (
-    id primary key,
+    id text primary key,
     share_id text not null,
     bsize integer not null,
-    is_uploaded boolean not null default false,
+    is_uploaded integer not null default 0,
     metadata text not null default '{}',
     foreign key (share_id) references share (id)
 ) strict, without rowid;
